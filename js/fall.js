e66f2a92d73f8fd44768009c2ff0631d2e0e0613
Nathan Farlow <natethegreatest11@gmail.com>
2022-06-12 19:43:31 -0700

    Add website

---
+++ js/fall.js
@@ -0,0 +1,12 @@
+
+function type() {
+    let target = "> Hacking. Food. Companies. Prizes.";
+    let text = document.getElementById('tagline').textContent;
+
+    if (text.length < target.length) {
+        document.getElementById('tagline').textContent = text + target[text.length];
+        setTimeout(type, 100);
+    }
+}
+
+window.onload = type;
