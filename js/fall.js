e939ed12eb61ed90422b19876995695743044fa7
Richard Liu <richy.liu.2002@gmail.com>
2022-06-18 20:24:49 -0700

    tweak styles, add navbar
    - add top navbar
    - fix styles for better mobile support

--- js/fall.js
+++ js/fall.js
@@ -1,11 +1,45 @@
-function type() {
-  let target = '> Hacking. Food. Companies. Prizes.';
-  let text = document.getElementById('tagline').textContent;
+function wait(delay) {
+  return new Promise(resolve => setTimeout(resolve, delay));
+}
 
-  if (text.length < target.length) {
-    document.getElementById('tagline').textContent = text + target[text.length];
-    setTimeout(type, 100);
+// start typewriter animation
+async function startTyping() {
+  const words = ['Hacking.', 'Food.', 'Companies.', 'Prizes.'];
+
+  let index = 0;
+  while (true) {
+    await typeWord(words[index]);
+    await wait(300);
+    index = (index + 1) % words.length;
   }
 }
 
-window.onload = type;
+// type one word of animation at a time
+async function typeWord(word) {
+  const tagline = document.getElementById('tagline');
+
+  for (const char of word) {
+    tagline.textContent += char;
+    await wait(150);
+  }
+
+  await wait(1000);
+
+  for (const char of word) {
+    tagline.textContent = tagline.textContent.slice(0, -1);
+    await wait(80);
+  }
+}
+
+// start the animation
+startTyping().then();
+
+// hide the scroll arrow when the page is scrolled
+window.addEventListener('scroll', function () {
+  const arrow = document.getElementById('arrow-container');
+  if (window.scrollY > 0) {
+    arrow.style.display = 'none';
+  } else {
+    arrow.style.display = 'block';
+  }
+});
