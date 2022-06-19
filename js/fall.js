3cd7c34d820104ca324f6024fdada8d7fc9fec36
Hassam Uddin <hassamuddin20@gmail.com>
2022-06-18 23:35:38 -0500

    create mobile nav bar

--- js/fall.js
+++ js/fall.js
@@ -43,3 +43,13 @@ window.addEventListener('scroll', function () {
     arrow.style.display = 'block';
   }
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
