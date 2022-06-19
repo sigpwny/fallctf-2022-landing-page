e0d70b2d144e9f01bd5def16f160da01051eaf37
Richard Liu <richy.liu.2002@gmail.com>
2022-06-18 18:18:14 -0700

    format with prettier

--- js/fall.js
+++ js/fall.js
@@ -1,12 +1,11 @@
-
 function type() {
-    let target = "> Hacking. Food. Companies. Prizes.";
-    let text = document.getElementById('tagline').textContent;
+  let target = '> Hacking. Food. Companies. Prizes.';
+  let text = document.getElementById('tagline').textContent;
 
-    if (text.length < target.length) {
-        document.getElementById('tagline').textContent = text + target[text.length];
-        setTimeout(type, 100);
-    }
+  if (text.length < target.length) {
+    document.getElementById('tagline').textContent = text + target[text.length];
+    setTimeout(type, 100);
+  }
 }
 
 window.onload = type;
